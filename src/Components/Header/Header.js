b435d94839c594531f2e1519652a1e94a4e27ff4
SameerDev12 <sameer.shahid@devsinc.com>
2022-12-16 21:58:12 +0500

    Header is Comleted

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,12 @@ import './Header.css';
 const Header = () => {
   return (
     <header className='o-header'>
-        <img src="https://react-builder-burger.firebaseapp.com/static/media/burger-logo.b8503d26.png" alt="Logo" />
+        <div className='o-header-logo-container'>
+          <img className='o-header-logo' src="https://react-builder-burger.firebaseapp.com/static/media/burger-logo.b8503d26.png" alt="Logo" />
+        </div>
         <nav className='n-navbar-menu'>
             <ul className='n-navbar-list'>
-               <li className='n-navbar-link'>Burger Builder</li>
+               <li className="n-navbar-link active-link">Burger Builder</li>
                <li className='n-navbar-link'>Login</li>
             </ul>
         </nav>
